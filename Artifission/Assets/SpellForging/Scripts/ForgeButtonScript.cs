2b6cc2a3724d6d0f145980f867408ecec78f4ec4
CollinNelson <collinnelson509@gmail.com>
2020-10-21 11:04:20 -0700

    Cleaned Up Code and DRASTICALLY improved rendering, lighting, and effects

---
+++ Artifission/Assets/SpellForging/Scripts/ForgeButtonScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ForgeButtonScript : MonoBehaviour
+{
+    private PlayerStatBindings playerBindings;
+
+    public SlotScript primaryFormNode;
+    public SlotScript primaryEffectNode;
+
+    void Update()
+    {
+        playerBindings = GetComponentInParent<SpellForgeController>().playerBindings;
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit) && hit.collider.gameObject == gameObject)
+            {
+                GameObject formRune = primaryFormNode.QueryForRune();
+                GameObject effectRune = primaryEffectNode.QueryForRune();
+
+                if(formRune != null && effectRune != null)
+                {
+                    SpellNodeScript newSpellNode = gameObject.AddComponent<SpellNodeScript>();
+                    newSpellNode.formPrefab = formRune;
+                    newSpellNode.effectObject = effectRune;
+                    playerBindings.SwapSpell(newSpellNode, 0);
+                    Destroy(newSpellNode);
+                }
+            }
+        }
+    }
+}
---
+++ Artifission/Assets/SpellForging/Scripts/SpellForgeInteractionScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellForgeInteractionScript : StandardInteractable
+{
+    public SpellForgeController spellForgeController;
+
+    protected override void Interact(GameObject player)
+    {
+        player.GetComponent<PlayerDeathHandler>().SetRespawnPosition(transform.position);
+        spellForgeController.SetShowState(true);
+    }
+}
