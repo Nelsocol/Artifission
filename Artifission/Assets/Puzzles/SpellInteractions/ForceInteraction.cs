86d0098b39d25a57a578a6b3be7f105c2fc019ef
CollinNelson <collinnelson509@gmail.com>
2022-02-07 17:14:11 -0800

    Deleting project to try and fix gitignore issues

--- Artifission/Assets/Puzzles/SpellInteractions/ForceInteraction.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ForceInteraction : ISpellInteractionType
-{
-    public int mForceStrength { get; set; }
-}
